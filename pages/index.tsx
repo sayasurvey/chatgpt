7920cabdf21fa55d00f29e319ed1d76b2294b44c
sayasurvey <sayasurvey@yahoo.co.jp>
2024-03-14 00:42:21 +0900

    FIX: UIをさらにChatGPT化

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Chat = () => {
     setError('');
 
     try {
-      const res = await axios.post('/api/chatgpt', { prompt }, { timeout: 15000 });
+      const res = await axios.post('/api/chatgpt', { prompt }, { timeout: 150000 });
       // 現在の質問と回答をhistoryに追加
       setHistory(history.concat([{ prompt, answer: res.data.text }]));
       setPrompt(''); // 質問を送信後、入力フィールドをクリア
@@ -30,47 +30,51 @@ const Chat = () => {
   };
 
   return (
-    <div className="mx-auto my-16 min-w-1/2 max-w-2xl px-4">
+    <div className="mx-auto my-7 min-w-1/2 max-w-2xl px-4">
       <div className="bg-gray-700 rounded-md md:flex md:items-center md:justify-between py-4 px-4">
         <div className="min-w-0 flex-1">
           <h2 className="text-2xl font-bold leading-7 text-white">Next ChatBot</h2>
         </div>
       </div>
-      <div className="px-4 py-5">
-        {isLoading && <div className="font-medium leading-6 text-lg text-indigo-700 pb-2">読み込み中...</div>}
-        {error && <div className="mt-4 text-red-500">{error}</div>}
-        {history.map((item, index) => (
-          <div key={index} className="mb-4">
-            <div className="font-medium leading-6 text-lg text-gray-900 pb-2">質問：</div>
-            <Markdown className="mt-2 text-gray-700" remarkPlugins={[remarkGfm]}>{item.prompt}</Markdown>
-            <div className="font-medium leading-6 text-lg text-gray-900 pb-2">回答：</div>
-            <Markdown className="mt-2 text-gray-700" remarkPlugins={[remarkGfm]}>{item.answer}</Markdown>
-          </div>
-        ))}
-        <div className="py-8">
-          <label htmlFor="question" className="block font-medium leading-6 text-lg text-gray-900 pb-2">
-            質問フォーム：
-          </label>
-          <div className="mt-2">
-            <textarea
-              id="question"
-              className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              placeholder="質問したいことを入力してください"
-              maxLength={500}
-              rows={5}
-              value={prompt}
-              onChange={(e) => setPrompt(e.target.value)}
-            />
-          </div>
+      <div className="flex flex-col h-[calc(100vh-8rem)] px-4 py-5">
+        <div className="overflow-auto">
+          {isLoading && <div className="font-medium leading-6 text-lg text-indigo-700 pb-2">読み込み中...</div>}
+          {error && <div className="mt-4 text-red-500">{error}</div>}
+          {history.map((item, index) => (
+            <div key={index} className="mb-4">
+              <div className="font-medium leading-6 text-lg text-gray-900 pb-2">質問：</div>
+              <Markdown className="mt-2 text-gray-700" remarkPlugins={[remarkGfm]}>{item.prompt}</Markdown>
+              <div className="font-medium leading-6 text-lg text-gray-900 pb-2">回答：</div>
+              <Markdown className="mt-2 text-gray-700" remarkPlugins={[remarkGfm]}>{item.answer}</Markdown>
+            </div>
+          ))}
         </div>
-        <div className="flex justify-end mb-8">
-          <button
-            className="rounded-md bg-indigo-600 px-6 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            disabled={isLoading || prompt.length === 0}
-            onClick={generateAnswer}
-          >
-            質問する
-          </button>
+        <div className="mt-auto">
+          <div className="py-2">
+            <label htmlFor="question" className="block font-medium leading-6 text-lg text-gray-900 pb-2">
+              質問フォーム：
+            </label>
+            <div className="mt-2">
+              <textarea
+                id="question"
+                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="質問したいことを入力してください"
+                maxLength={500}
+                rows={5}
+                value={prompt}
+                onChange={(e) => setPrompt(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="flex justify-end">
+            <button
+              className="rounded-md bg-indigo-600 px-6 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              disabled={isLoading || prompt.length === 0}
+              onClick={generateAnswer}
+            >
+              質問する
+            </button>
+          </div>
         </div>
       </div>
     </div>
